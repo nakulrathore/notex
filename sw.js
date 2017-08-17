6bb5dca4ca6603e39d2196b5251f333975af165d
Nakul <narendrasingh@outlook.in>
2017-08-17 23:26:09 +0530

    update start url

--- sw.js
+++ sw.js
@@ -4,8 +4,8 @@ importScripts('sw-toolbox.js');
 
 toolbox.precache(["index.html","style.css"]);
 
-toolbox.router.get('images/*', toolbox.cacheFirst);
+toolbox.router.get('/*', toolbox.cacheFirst);
 
-toolbox.router.get('*', toolbox.networkFirst, {
-  networkTimeoutSeconds: 5
+toolbox.router.get('/*', toolbox.networkFirst, {
+  networkTimeoutSeconds: 2
 });
